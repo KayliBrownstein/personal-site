1793e665e7040896fc85fe37834c89e95dd177e6
KayliBrownstein <kaylivbrownstein@gmail.com>
2017-05-26 16:23:46 -0400

    reformatted profex container to const

--- react/containers/ProfessionalExperienceContainer.js
+++ react/containers/ProfessionalExperienceContainer.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
 import ProfExTile from '../components/ProfExTile';
 
-class ProfessionalExperienceContainer extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-
-    }
-  }
-
-  render(){
+const ProfessionalExperienceContainer = props => {
     return(
       <div className="profex-container small-12 medium-12 large-12 columns">
         <h3>Professional Experience</h3>
         <ProfExTile />
       </div>
     )
-  }
 }
 
 export default ProfessionalExperienceContainer;
