b65acc17c81605cbc3e0fe61332b8b94f33f93ed
KayliBrownstein <kaylivbrownstein@gmail.com>
2017-06-04 18:56:58 -0400

    cleaned navbar

--- react/components/Navbar.js
+++ react/components/Navbar.js
@@ -8,13 +8,9 @@ const Navbar = props => {
     <div className ="navbar">
       <ul className="links">
         <li onClick={(e) => goToAnchor("home", true)}>Home</li>
-
         <li onClick={(e) => goToAnchor("projects-section", true)}>Projects</li>
-
         <li onClick={(e) => goToAnchor("resume-section", true)}>Resume</li>
-
         <li onClick={(e) => goToAnchor("connect-section", true)}>Connect</li>
-
         <li onClick={(e) => goToAnchor("contact-section", true)}>Contact</li>
       </ul>
       {props.children}
