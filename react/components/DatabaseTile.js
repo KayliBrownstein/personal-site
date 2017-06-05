a92032f10f4283c48af98c8ad8fb6b697f877c0c
KayliBrownstein <kaylivbrownstein@gmail.com>
2017-06-05 13:33:40 -0400

    fix smaller tile icons

--- react/components/DatabaseTile.js
+++ react/components/DatabaseTile.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DatabaseTile = props => {
   return(
-    <div className="database-skill-tile small-2 medium-2 large-2 columns">
+    <div className="database-skill-tile small-2 medium-4 large-4 columns">
       <img src={props.icon} width='100' height='150'/>
     </div>
   )
